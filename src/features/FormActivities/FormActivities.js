04104b3303d153f3ac1a964d67d30311a6246eb2
Juan <juanjoseborrelli92@outlook.com>
2021-09-30 20:47:24 -0300

    Agregando los campos requeridos del formulario

--- src/features/FormActivities/FormActivities.js
+++ src/features/FormActivities/FormActivities.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  VStack,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 
 export const FormActivities = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [value, setValue] = React.useState("");
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  };
+
+  return (
+    <VStack mt="2rem">
+      <Container maxW="container.md" className="editFormCreationActivities">
+        <FormControl mb="1rem" id="name" isRequired>
+          <FormLabel>Nombre</FormLabel>
+          <Input placeholder="Nombre de actividad" />
+        </FormControl>
+
+        <Text mb="8px">Descripción: </Text>
+        <Textarea
+          value={value}
+          onChange={handleInputChange}
+          placeholder="Descripción de actividad"
+          size="md"
+          variant="outline"
+          mb="1rem"
+        />
+        
+        <FormControl mb="1rem" id="image" isRequired>
+          <FormLabel>Imagen</FormLabel>
+          <input type="file" accept=".png, .jpg" />
+        </FormControl>
+      </Container>
+    </VStack>
+  );
+};
