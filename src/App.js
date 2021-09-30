e4df05059fdd9b661eb19c81d9e358c48dc6c806
Juan <juanjoseborrelli92@outlook.com>
2021-09-30 20:32:47 -0300

    Implementacion de ChakraProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import { FormActivities } from "./features/FormActivities/FormActivities";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return <FormActivities />;
+  return (
+    <ChakraProvider>
+      <FormActivities />;
+    </ChakraProvider>
+  );
 }
 
 export default App;
