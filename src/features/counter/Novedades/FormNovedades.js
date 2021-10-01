e215f4f458dfc8deeb99f941abfef5e28b1fb190
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-01 15:22:56 -0300

    form title ,  button added and validation:El título debe contener una longitud mínima de 4 caracteres

--- src/features/counter/Novedades/FormNovedades.js
+++ src/features/counter/Novedades/FormNovedades.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Form, Formik } from 'formik';
 
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { Heading } from '@chakra-ui/layout';
+import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Button } from '@chakra-ui/button';
+
+// validaciones
+const formSchema = Yup.object().shape({
+  title: Yup.string()
+        .min(4,"Se requieren 4 caracteres como mínimo")
+        .required("Requerido"),
+});
 
 
 
@@ -15,13 +28,27 @@ const Formnovedades = () => {
         category:"",
         image:""
       }}
+      validationSchema={formSchema}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
       {(formik) => (
         <Form className="loginForm">
-
+        <Heading m={4}>Formulario Novedades</Heading>
+          <Field name="title">
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.title && form.touched.title}>
+                <FormLabel htmlFor="title">Titulo</FormLabel>
+                <Input {...field} id="title" placeholder="Titulo" />
+                <FormErrorMessage>{form.errors.title}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          
+          <Button mt={4} colorScheme="teal" type="submit">
+            Ingresar
+          </Button>
         </Form>
       )}
     </Formik>
