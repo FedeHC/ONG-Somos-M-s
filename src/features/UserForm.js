dd8756ebd2f9afd988b61003c26031c8e20c9538
facc77 <ferrerfacundo96@gmail.com>
2021-10-02 11:44:43 -0300

    added testObject and conditional component behavior

--- src/features/UserForm.js
+++ src/features/UserForm.js
@@ -25,14 +25,22 @@ const SignupSchema = Yup.object().shape({
       return file && (file.type === "image/jpg" || file.type === "image/png");
     }),
 });
+/*   const objetoPrueba = {
+    name: "facu",
+    email: "facuf@gmail.com",
+    role: "Usuario",
+    image: undefined,
+    id: 1,
+  }; */
+let objetoPrueba;
 
 const LoginForm = () => {
   return (
     <Formik
       initialValues={{
-        name: "",
-        email: "",
-        role: "",
+        name: objetoPrueba ? objetoPrueba.name : "",
+        email: objetoPrueba ? objetoPrueba.email : "",
+        role: objetoPrueba ? objetoPrueba.role : "",
         image: undefined,
       }}
       validationSchema={SignupSchema}
@@ -42,12 +50,18 @@ const LoginForm = () => {
     >
       {({ errors, touched }) => (
         <Form className="loginForm">
-          <Heading m={4}>"Crear usuario"</Heading>
+          <Heading m={4}>
+            {objetoPrueba ? "Editar usuario" : "Crear usuario"}
+          </Heading>
           <Field name="name">
             {({ field, form }) => (
               <FormControl isInvalid={form.errors.name && form.touched.name}>
                 <FormLabel htmlFor="name">Name</FormLabel>
-                <Input {...field} id="name" />
+                <Input
+                  {...field}
+                  id="name"
+                  placeholder={objetoPrueba ? objetoPrueba.name : "name"}
+                />
                 <FormErrorMessage>{form.errors.name}</FormErrorMessage>
               </FormControl>
             )}
@@ -56,7 +70,11 @@ const LoginForm = () => {
             {({ field, form }) => (
               <FormControl isInvalid={form.errors.email && form.touched.email}>
                 <FormLabel htmlFor="email">Email</FormLabel>
-                <Input {...field} id="email" />
+                <Input
+                  {...field}
+                  id="email"
+                  placeholder={objetoPrueba ? objetoPrueba.email : "email"}
+                />
                 <FormErrorMessage>{form.errors.email}</FormErrorMessage>
               </FormControl>
             )}
@@ -68,7 +86,11 @@ const LoginForm = () => {
                 isInvalid={form.errors.role && form.touched.role}
               >
                 <FormLabel>Rol</FormLabel>
-                <Select {...field} id="role">
+                <Select
+                  {...field}
+                  id="role"
+                  placeholder={objetoPrueba ? objetoPrueba.role : "role"}
+                >
                   <option value="Administrador">Administrador</option>
                   <option value="Usuario">Usuario</option>
                 </Select>
