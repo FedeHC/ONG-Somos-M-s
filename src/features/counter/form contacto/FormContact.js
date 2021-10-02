2842cdc036d9f73d2d099dd5efd7d0fa83fedfd1
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-02 13:10:23 -0300

    form structure added

--- src/features/counter/form contacto/FormContact.js
+++ src/features/counter/form contacto/FormContact.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Form, Formik } from 'formik';
+
+
 
 const Formcontact = () => {
  return (
   <div>
+    <Formik
+      initialValues={{
+        
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => (
+        <Form className="form-contact">
+          
+        </Form>
+      )}
+    </Formik>
    
   </div>
  );
