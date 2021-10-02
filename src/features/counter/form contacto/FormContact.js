26f753a8fa7e869edf177983b07c7e18566917ec
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-02 13:27:30 -0300

    input name added and validation name

--- src/features/counter/form contacto/FormContact.js
+++ src/features/counter/form contacto/FormContact.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import { Form, Formik } from 'formik';
 
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { Heading } from '@chakra-ui/layout';
+import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Button } from '@chakra-ui/button';
+import { Center } from "@chakra-ui/react"
+
+
+// validaciones
+const ContactSchema = Yup.object().shape({
+ name: Yup.string()
+       .required("Requerido"),
+});
 
 
 const Formcontact = () => {
  return (
-  <div>
+  <Center>
     <Formik
       initialValues={{
-        
+       name:"",
+       email:"",
+       phone:Number,
+       message:""
       }}
+      validationSchema={ContactSchema}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
       {(formik) => (
         <Form className="form-contact">
-          
+        <Heading m={4}>Contactate con nosotros</Heading>
+          <Field name="name">
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.name && form.touched.name}>
+                <FormLabel htmlFor="name">Nombre</FormLabel>
+                <Input {...field} id="name" placeholder="Nombre" />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+          <Button mt={4} colorScheme="teal" type="submit">
+            Enviar
+          </Button>
         </Form>
       )}
     </Formik>
    
-  </div>
+  </Center>
  );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Formcontact from './features/counter/form contacto/FormContact';
+import { ChakraProvider } from "@chakra-ui/react"
 
 ReactDOM.render(
   <React.StrictMode>
+   <ChakraProvider>
     <Provider store={store}>
-      <App />
+      <Formcontact />
     </Provider>
+   </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
