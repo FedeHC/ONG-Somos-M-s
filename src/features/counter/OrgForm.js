22c5b1498c874c568c2eb7d64b00a707458e9867
FedeHC <fede.hc@mail.com>
2021-10-02 16:26:00 -0300

    Creating OrgForm component.

---
+++ src/features/counter/OrgForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+
+function OrgForm() {
+  return(
+    
+  );  
+}
+
+export default OrgForm;
