a8e2fcd299c8246020540eea2f2ca246d63a7856
facc77 <ferrerfacundo96@gmail.com>
2021-10-05 20:34:14 -0300

    creation of file News/Detail

---
+++ src/features/News/Detail/NewsDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "./Title.js";
+
+const NewsDetail = (props) => {
+  return (
+    <Title text="Novedad 1" image="https://fondosmil.com/fondo/17010.jpg" />
+  );
+};
+
+export default NewsDetail;
