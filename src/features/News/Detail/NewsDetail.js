f1df63b80de2a4846a41f0814a781c4a0aca08bd
facc77 <ferrerfacundo96@gmail.com>
2021-10-05 20:49:05 -0300

    added SimpleGrid for image and description

--- src/features/News/Detail/NewsDetail.js
+++ src/features/News/Detail/NewsDetail.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Title from "./Title.js";
+import { Grid, GridItem, Box, SimpleGrid } from "@chakra-ui/react";
 
 const NewsDetail = (props) => {
   return (
-    <Title text="Novedad 1" image="https://fondosmil.com/fondo/17010.jpg" />
+    <>
+      <Title text="Novedad 1" image="https://fondosmil.com/fondo/17010.jpg" />
+      <SimpleGrid columns={2} spacing={30}>
+        <Box bg="tomato" height="80px">
+          imagen
+        </Box>
+        <Box bg="tomato" height="80px" padding="10px">
+          <h1>Detalles de novedad</h1>
+        </Box>
+      </SimpleGrid>
+    </>
   );
 };
 
