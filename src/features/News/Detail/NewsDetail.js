42b3fee6cd3846f4dca42c73c52eebed0bed807f
facc77 <ferrerfacundo96@gmail.com>
2021-10-06 18:25:40 -0300

    font style fixed

--- src/features/News/Detail/NewsDetail.js
+++ src/features/News/Detail/NewsDetail.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Title from "./Title.js";
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Divider } from "@chakra-ui/react";
 import "./NewsDetail.scss";
 
 const NewsDetail = (props) => {
   let objetoPrueba = {
-    titulo: "novedad3",
+    titulo: "novedad numero 7",
     textoNovedad:
       " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis, nisi vel laoreet condimentum, velit lacus aliquet lectus, non finibus enim turpis id augue. Sed venenatis sapien ac justo porttitor euismod. Phasellus lobortis eu justo ac euismod. Vivamus at metus et sapien malesuada aliquam ac eget odio. Pellentesque quis varius urna. ",
     imagen:
@@ -15,15 +15,25 @@ const NewsDetail = (props) => {
     <>
       <Title
         text={objetoPrueba.titulo}
-        image="https://fondosmil.com/fondo/17010.jpg"
+        /*         image="https://fondosmil.com/fondo/17010.jpg"
+         */
       />
-      <SimpleGrid columns={2} spacing={50} margin={20} className="simpleGrid">
-        <Box height="80px" padding="10px">
+      <SimpleGrid
+        columns={1}
+        minChildWidth="250px"
+        spacing={50}
+        margin={20}
+        className="simpleGrid"
+      >
+        <Box height="100%" padding="10px">
           <img src={objetoPrueba.imagen} className="newsImage" alt="" />
         </Box>
-        <Box height="100%" padding="10px" className="newsText">
-          <h1>Detalles de novedad</h1>
-          <h3>{objetoPrueba.textoNovedad}</h3>
+        <Box padding="10px">
+          <div className="newsText">
+            <p className="title">Detalles de novedad</p>
+            <Divider />
+            <p className="content">{objetoPrueba.textoNovedad}</p>
+          </div>
         </Box>
       </SimpleGrid>
     </>
