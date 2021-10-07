f3808da2ed497dbd46e74e09d918f1afe75653c1
facc77 <ferrerfacundo96@gmail.com>
2021-10-07 17:20:24 -0300

    added react-icon to contact list

--- src/features/contactSection/Contact.js
+++ src/features/contactSection/Contact.js
@@ -1,15 +1,42 @@
 import React from "react";
-import Title from "./Title";
-import "./contact.scss";
+/* import Title from "./Title";
+ */ import "./contact.scss";
+import { FaFacebookSquare, FaPhoneAlt } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { FiMail } from "react-icons/fi";
 
 const Contact = () => {
+  let objeto = {
+    mail: "somosfundacionmas@gmail.com",
+    instagram: "SomosMas",
+    facebook: "SomosMas",
+    phone: 111232312,
+    linkedin: "somosMas",
+    twitter: "SomosMas",
+  };
   return (
     <>
-      <Title text="Contacto" />
       <div className="container">
         <div id="info">
           <h1>Contactate con Nosotros!</h1>
           <h1>Nos interesa saber tu opinión sobre nuestras actividades!</h1>
+          <ul className="list">
+            <li>
+              <FaFacebookSquare className="facebook" />
+              Facebook: www.facebook.com/{objeto.facebook}
+            </li>
+            <li>
+              <GrInstagram className="instagram" />
+              Instagram: www.instagram.com/{objeto.instagram}
+            </li>
+            <li>
+              <FiMail className="mail" />
+              Mail: {objeto.mail}
+            </li>
+            <li>
+              <FaPhoneAlt className="phone" /> Telefono: {objeto.phone}
+            </li>
+          </ul>
         </div>
         <div id="map">map</div>
         <div id="contactForm">form</div>
