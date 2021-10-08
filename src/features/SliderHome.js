604f16d0ee8af38f9a59931fd4d9a5411ac83120
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-08 00:09:18 -0300

    data and structure sliders

--- src/features/SliderHome.js
+++ src/features/SliderHome.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+//data example
+const data = [
+  {image:'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+   title:"tes1",
+   description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."    
+  },
+  {image:'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80',
+   title:"test2",
+   description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."    
+  },
+  {image:'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+   title:"test3",
+   description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."    
+  },
+  {image:'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+   title:"test4",
+   description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."    
+  },
+]
+
+
 const Sliderhome = () => {
- return (
-  <div>
-   
-  </div>
- );
+  return (
+    <div>
+      <Slider  >
+          
+        </Slider>
+    </div>
+  );
 }
 
 export default Sliderhome;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import SliderHome from './features/SliderHome';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SliderHome />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
