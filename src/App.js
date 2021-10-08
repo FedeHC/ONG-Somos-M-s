e76a640ac10ef94ee0a308ae281dd1079faf3265
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-07 23:32:33 -0300

    component card created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import NewsDetail from './features/activities/detail/Detail';
+import Card from './features/Card';
 
 function App() {
   return (
     <div className="App">
-      <ActivitiesDetail />
+      <Card />
     </div>
   );
 }
