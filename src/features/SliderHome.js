487a8fb809e276e304fe08947ebc87feeb3b1bff
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-08 00:16:29 -0300

    style carousel-container added

--- src/features/SliderHome.js
+++ src/features/SliderHome.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./sliderHome.css";
 
 //icons
 import { IoChevronForwardOutline, IoChevronBackOutline } from "react-icons/io5";
