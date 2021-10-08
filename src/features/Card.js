df44df37a97e5a5e00cd9a96e3b383e543679ed8
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-07 23:36:23 -0300

    card structure added

--- src/features/Card.js
+++ src/features/Card.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+//style
+import "./card.css";
+
+
+//data example img , title and description
+const datosApi ={
+  image:"https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+  title: "prueba de titulo",
+  description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+}
+
 const Card = () => {
   return (
     <div>
-      
+      <figure className="card">
+      <img src={datosApi.image} alt={datosApi.title} />
+      <figcaption>
+        <h3>{datosApi.title}</h3>
+        <p>
+         {datosApi.description}
+        </p>
+      </figcaption>
+    </figure>
     </div>
   );
 }
