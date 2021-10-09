a83311f1ba87e0f11a82bff7951d83adbf458c42
FedeHC <fede.hc@mail.com>
2021-10-09 16:48:18 -0300

    Removing Table component from UsersList.

--- src/pages/UsersList/UsersList.js
+++ src/pages/UsersList/UsersList.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { ChakraProvider, Container, SimpleGrid, Link,
-         Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { ChakraProvider, Container, SimpleGrid, Link } from "@chakra-ui/react";
+import UsersTable from "./UsersTable";
 
 
 const UsersList = () => {
@@ -14,50 +14,20 @@ const UsersList = () => {
           {/* CREATE USER LINK */}
           <Link href="/backoffice/users/create"
                 bg="gray.100"
-                padding={5}>Create User</Link>
+                padding={2}><b>Create User</b></Link>
 
-          {/* TABLE */}
-          <Table variant="striped"
-                 colorScheme="gray">
+          {/* USERS TABLE */}
+          {/* Using mocking data */}
+          <UsersTable data={[ { id: 0, 
+                                name: "User1",
+                                email: "user.one@mail.com"},
+                              { id: 1, 
+                                name: "User2",
+                                email: "user.two@mail.com"},
+                              { id: 2, 
+                                name: "User3",
+                                email: "user.three@mail.com"} ]} />
 
-            {/* TABLE HEAD */}
-            <Thead>
-              <Tr>
-                <Th>Name</Th>
-                <Th>Email</Th>
-                <Th>Actions</Th>
-              </Tr>
-            </Thead>
-
-            {/* TABLE BODY */}
-            <Tbody>
-              <Tr>
-                <Td>User1</Td>
-                <Td>user.one@mail.com</Td>
-                <Td>
-                <Link href="/backoffice/users/create"
-                      bg="yellow.100"
-                      padding={3}
-                      marginBottom={30}>Editar</Link>
-
-                <Link href="/backoffice/users/create"
-                      bg="red.100"
-                      padding={3}
-                      marginBottom={30}>Eliminar</Link>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>User2</Td>
-                <Td>user.two@mail.com</Td>
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td>User3</Td>
-                <Td>user.three@mail.com</Td>
-                <Td></Td>
-              </Tr>
-            </Tbody>
-          </Table>
         </SimpleGrid>
       </Container>
     </ChakraProvider>
