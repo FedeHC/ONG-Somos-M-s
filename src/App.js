e3f84ca1fafff6b1504f10517575f02a53f88d72
Joel <teslavargas18@gmail.com>
2021-10-09 23:11:49 -0300

    a

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import NewsDetail from './features/activities/detail/Detail';
 
 function App() {
   return (
     <div className="App">
-      <ActivitiesDetail />
+      
     </div>
   );
 }
