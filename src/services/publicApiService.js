e1888a2b76a29562a3df7c24112793b07982ae6e
Adriel Baez <baez.adriel27@gmail.com>
2021-10-12 11:41:58 -0300

    public api service, I add a url example to make requests

--- src/services/publicApiService.js
+++ src/services/publicApiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // GET
 export const methodGetPublic = async (endpoint, id = null) => {
   try {
-    const response = await axios.get(`${url}/${endpoint}/${id || ""}`);
+    const response = await axios.get(`${'url'}/${endpoint}/${id || ""}`);
     console.log(response);
     return response;
   } catch (error) {
