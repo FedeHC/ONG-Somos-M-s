6a5074985547984ac5093943cf691b74df7e1706
Adriel Baez <baez.adriel27@gmail.com>
2021-10-12 17:04:11 -0300

    I add the get method for making public requests

--- src/services/publicApiService.js
+++ src/services/publicApiService.js
@@ -4,9 +4,8 @@ import axios from 'axios';
 export const methodGetPublic = async (endpoint, id = null) => {
   try {
     const response = await axios.get(`${'url'}/${endpoint}/${id || ""}`);
-    console.log(response);
     return response;
   } catch (error) {
-    console.error(error);
+    return error;
   }
 };
