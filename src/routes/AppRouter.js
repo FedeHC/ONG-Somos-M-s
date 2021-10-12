fd6f1758001d0a9b2a823df7bcd69e859e84000a
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-12 17:51:02 -0300

    AppRouter created

---
+++ src/routes/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const AppRouter = () => {
+ return (
+  <Router>
+  <div>
+    <Switch>
+        
+    </Switch>
+  </div>
+  </Router>
+ );
+}
+
+export default AppRouter;
