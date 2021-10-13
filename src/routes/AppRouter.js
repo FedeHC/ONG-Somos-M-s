07e75d6465ffae11462e8d9d660ce5562e76f8c7
Alejandro Villafañe <info@alezvi.com.ar>
2021-10-12 23:15:44 -0300

    merge

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ScreenSlidesList from '../pages/screenSlidesList';
+import HomeForm from "../features/HomeForm/HomeForm";
+import MemberForm from "../features/MemberForm/MemberForm";
 
 const AppRouter = () => {
- return (
-  <Router>
-  <div>
-    <Switch>
-        <Route exact path="/backoffice/slides" component={ScreenSlidesList} /> 
-    </Switch>
-  </div>
-  </Router>
- );
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/backoffice/slides" component={ScreenSlidesList}/>
+          <Route exact path="/backoffice/home" component={HomeForm}/>
+          <Route exact path="/backoffice/members/edit" component={MemberForm}/>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default AppRouter;
