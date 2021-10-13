d148304979147feae60e9fed753d20fc642c31fb
Juan <juanjoseborrelli92@outlook.com>
2021-10-12 23:31:30 -0300

    Agregando el metodo tokenValidate

--- src/features/methods/methodDeletePrivate .js
+++ src/features/methods/methodDeletePrivate .js
@@ -1,10 +1,13 @@
-export const methodDeletePrivate  = async (endpoint, id = null, headers) => {
+import { tokenValidate } from "./tokenValidate";
+
+export const methodDeletePrivate = async (endpoint, id = null) => {
+
   try {
     if (!id) {
       throw new Error("No se encontró un id en la petición");
     }
     const response = await axios.delete(`${url}/${endpoint}/${id}`, {
-      headers,
+      tokenValidate()
     });
     return response;
   } catch (error) {
