389aa43d17d509ca48a725113dc844eb402fd14d
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-16 15:03:33 -0300

    getContact added

--- src/services/ApiContact.js
+++ src/services/ApiContact.js
@@ -1,7 +1,12 @@
-import {methodPostPublic} from "./publicApiService";
+import {methodGetPublic, methodPostPublic} from "./publicApiService";
 
 
 export const postContact = async(body) =>{
    const resp = await methodPostPublic("contacts",body);
    return resp;
+}
+
+export const getContact = async(body) =>{
+ const resp = await methodGetPublic("contacts");
+ return resp;
 }
