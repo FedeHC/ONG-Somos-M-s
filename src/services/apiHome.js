3fa6a8391e44490b2dd361fa312995be9b665b9e
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-16 23:42:36 -0300

    postHome added

--- src/services/apiHome.js
+++ src/services/apiHome.js
@@ -1,7 +1,12 @@
-import { methodGetPublic } from "./publicApiService"
+import { methodGetPublic, methodPostPublic } from "./publicApiService"
 
 
 export const getHome = async() => {
   const resp = await methodGetPublic("organization");
+  return resp;
+};
+
+export const postHome = async(body) => {
+  const resp = await methodPostPublic("organization", body);
   return resp;
 };
