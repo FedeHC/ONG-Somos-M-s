470a36cc92cc63812057d0d75c38a4122ee92408
FedeHC <fede.hc@mail.com>
2021-10-17 13:32:04 -0300

    Adding route for activities/create and commenting route with missing component.

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -13,46 +13,32 @@ import NewsScreen from "../pages/NewsScreen/NewsScreen";
 import SlideForm from "../features/SlideForm/SlideForm";
 import ActivitiesList from "../features/activitiesList/ActivitiesList";
 import Home from "../pages/home";
+import FormActivities from "../features/FormActivities/FormActivities";
+
 
 const AppRouter = () => {
   return (
     <Router>
       <div>
         <Switch>
+          {/* USERS */}
           <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path="/backoffice/slidesScreen"
-            component={SlidesScreen}
-          />
-          <Route
-            exact
-            path="/backoffice/slidesScreen/form"
-            component={SlideForm}
-          />
           <Route exact path="/contacto" component={Contact} />
-          <Route exact path="/backoffice/slides" component={ScreenSlidesList} />
-          <Route exact path="/backoffice/home" component={HomeForm} />
-          <Route exact path="/backoffice/members/edit" component={MemberForm} />
-          <Route
-            exact
-            path="/backoffice/categories"
-            component={CategoriesBackOffice}
-          />
-          <Route path="/backoffice/organization" component={Organization} />
-          <Route
-            exact
-            path="backoffice/testimonials"
-            component={TestimonialForm}
-          />
           <Route exact path="/donar" component={Donations} />
           <Route exact path="/gracias" component={Gracias} />
+
+          {/* BACKOFFICE */}
+          <Route exact path="/backoffice/home" component={HomeForm} />
+          <Route exact path="/backoffice/slidesScreen" component={SlidesScreen} />
+          <Route exact path="/backoffice/slidesScreen/form" component={SlideForm} />
+          {/* <Route exact path="/backoffice/slides" component={ScreenSlidesList} /> */}
+          <Route exact path="/backoffice/members/edit" component={MemberForm} />
+          <Route exact path="/backoffice/categories" component={CategoriesBackOffice} />
+          <Route exact path="/backoffice/organization" component={Organization} />
+          <Route exact path="/backoffice/testimonials" component={TestimonialForm} />
           <Route exact path="/backoffice/news" component={NewsScreen} />
-          <Route
-            exact
-            path="/backoffice/activities"
-            component={ActivitiesList}
-          />
+          <Route exact path="/backoffice/activities" component={ActivitiesList} />
+          <Route exact path="/backoffice/activities/create" component={FormActivities} />
         </Switch>
       </div>
     </Router>
