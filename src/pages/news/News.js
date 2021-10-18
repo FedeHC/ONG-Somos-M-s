67ad9154e715f1b7821e4ad25bdcb3bbea614d02
facc77 <ferrerfacundo96@gmail.com>
2021-10-18 12:37:31 -0300

    added GET services to News components

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import "./News.css";
+import axios from "axios";
 
 const News = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://ongapi.alkemy.org/api/news")
+      .then((response) => {
+        setNewsList(response.data.data);
+        console.log(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <div>
-          <h1>Acá va el título</h1>
+        <h1>Acá va el título</h1>
       </div>
       <div className="containerCard">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {newsList.map((news) => (
+          <Card news={news} />
+        ))}
       </div>
     </div>
   );
