b07bf3908f78a81755b6cf3854bf9b85101dce57
Adriel Baez <baez.adriel27@gmail.com>
2021-10-18 10:55:40 -0300

    update endpoints in apiCategories.js file with .env file

--- src/services/apiCategories.js
+++ src/services/apiCategories.js
@@ -1,18 +1,18 @@
 import { methodPostPrivate, methodPutPrivate } from "./privateApiService";
 import { methodGetPublic } from "./publicApiService"
-
+const endpointCategories = process.env.REACT_APP_ENDPOINT_CATEGORIES;
 
 export const getCategories = async(id = null) => {
-  const resp = await methodGetPublic("categories", id);
+  const resp = await methodGetPublic(endpointCategories, id);
   return resp;
 };
 
 export const postCategory = async(body, headers) => {
-  const resp = await methodPostPrivate("categories", body, headers);
+  const resp = await methodPostPrivate(endpointCategories, body, headers);
   return resp;
 };
 
 export const putCategory = async(body, headers) => {
-  const resp = await methodPutPrivate("categories", body, headers);
+  const resp = await methodPutPrivate(endpointCategories, body, headers);
   return resp;
 };
