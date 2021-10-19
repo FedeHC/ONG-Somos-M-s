046ee1e84a80f782e081bae9c83055353732f034
Joel <teslavargas18@gmail.com>
2021-10-19 13:44:12 -0300

    peticiones centralizadas

---
+++ src/services/apiMembers.js
@@ -0,0 +1,14 @@
+import { methodDeletePrivate, methodPostPrivate } from "./privateApiService";
+import { methodGetPublic } from "./publicApiService";
+
+export const getMembers = async (id = null) => {
+  return await methodGetPublic("members", id);
+};
+
+export const deleteMembers = async (id) => {
+  return await methodDeletePrivate("members", id);
+};
+
+export const createMember = async (body) => {
+  return await methodPostPrivate("members", body);
+};
