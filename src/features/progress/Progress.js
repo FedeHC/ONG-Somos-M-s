3597d5041319cea1312f820e3c36ffaefe879bd9
facc77 <ferrerfacundo96@gmail.com>
2021-10-19 16:20:57 -0300

    progress folder created

---
+++ src/features/progress/Progress.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Progress } from "@chakra-ui/react";
+
+const Progress = () => {
+  return <Progress size="xs" isIndeterminate />;
+};
+
+export default Progress;
