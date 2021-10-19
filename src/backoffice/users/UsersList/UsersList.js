059daeebfda27968e855acf4c07ba86fdace7faa
facc77 <ferrerfacundo96@gmail.com>
2021-10-19 01:54:03 -0300

    get, put and delete method added to list

--- src/backoffice/users/UsersList/UsersList.js
+++ src/backoffice/users/UsersList/UsersList.js
@@ -1,28 +1,30 @@
 import React from "react";
-import { ChakraProvider, Container, SimpleGrid, Button } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Container,
+  SimpleGrid,
+  Button,
+} from "@chakra-ui/react";
 import UsersTable from "./UsersTable";
 
-
-const UsersList = ({ data }) => {
+const UsersList = () => {
   return (
-    <ChakraProvider>
-      <Container>
-        <SimpleGrid columns={1}
-                    spacing={10}
-                    marginTop={10}>
+    <Container>
+      <SimpleGrid columns={1} spacing={10} marginTop={10}>
+        {/* CREATE USER LINK */}
+        <Button
+          href="/backoffice/users/create"
+          type="button"
+          colorScheme="gray"
+          padding={2}
+        >
+          Create User
+        </Button>
 
-          {/* CREATE USER LINK */}
-          <Button href="/backoffice/users/create"
-                  type="button"
-                  colorScheme="gray"
-                  padding={2}>Create User</Button>
-
-          {/* USERS TABLE */}
-          <UsersTable data={data}/>
-
-        </SimpleGrid>
-      </Container>
-    </ChakraProvider>
+        {/* USERS TABLE */}
+        <UsersTable />
+      </SimpleGrid>
+    </Container>
   );
 };
 
--- src/backoffice/users/UsersList/UsersTable.js
+++ src/backoffice/users/UsersList/UsersTable.js
@@ -1,43 +1,71 @@
-import React from "react";
-import { Link, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { getUsers, deleteUser } from "../../../services/apiUsers";
+import {
+  Link,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Button,
+} from "@chakra-ui/react";
 
+const UsersTable = () => {
+  const [userList, setUserList] = useState([]);
+  useEffect(() => {
+    getUsers().then((response) => setUserList(response.data.data));
+    console.log(userList);
+  }, []);
 
-const UsersTable = ({ data }) => {
+  const editUser = (id) => {
+    window.location.href = `/backoffice/users/edit/${id}`;
+  };
+
+  const deleteUsers = (id) => {
+    deleteUser(id);
+  };
+
   return (
-    <Table variant="striped"
-          colorScheme="gray">
-
-    {/* TABLE HEAD */}
-    <Thead>
-      <Tr>
-        <Th>Name</Th>
-        <Th>Email</Th>
-        <Th>Actions</Th>
-      </Tr>
-    </Thead>
-
-    {/* TABLE BODY */}
-    <Tbody>
-      {data.map( (user) => 
-        <Tr key={user.id}>
-          <Td>{user.name}</Td>
-          <Td>{user.email}</Td>
-          <Td>
-          <Link href=""
-                color="yellow.600"
-                padding={1}
-                marginRight={3}
-                marginBottom={30}><b>Editar</b></Link>
-
-          <Link href=""
-                color="red.600"
-                padding={1}
-                marginBottom={30}><b>Eliminar</b></Link>
-          </Td>
+    <Table variant="striped" colorScheme="gray">
+      {/* TABLE HEAD */}
+      <Thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th>Email</Th>
+          <Th>Actions</Th>
         </Tr>
-      )}
-    </Tbody>
-  </Table>
+      </Thead>
+
+      {/* TABLE BODY */}
+      <Tbody>
+        {userList.map((user) => (
+          <Tr key={user.id}>
+            <Td>{user.name}</Td>
+            <Td>{user.email}</Td>
+            <Td>
+              <Button
+                href=""
+                color="yellow.600"
+                marginRight={3}
+                onClick={() => editUser(user.id)}
+              >
+                <b>Editar</b>
+              </Button>
+
+              <Button
+                href=""
+                color="red.600"
+                onClick={() => deleteUsers(user.id)}
+              >
+                <b>Eliminar</b>
+              </Button>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
 };
 
