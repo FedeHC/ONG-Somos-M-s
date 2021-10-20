1729e8bb825693bfe4dedf1a091dd09e9f16cfd5
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-19 23:34:58 -0300

    MapContainer and TitleLayer added

--- src/pages/Contact/map/MapView.js
+++ src/pages/Contact/map/MapView.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { MapContainer , TileLayer } from "react-leaflet";
+
 
 const Mapview = () => {
  return (
   <div>
-    
+    <MapContainer center={[51.505, -0.09]} zoom={15} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
   </div>
  );
 }
