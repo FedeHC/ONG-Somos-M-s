3cb3d44e43f79e4d4137e05e073768a3668464aa
FedeHC <fede.hc@mail.com>
2021-10-21 13:54:11 -0300

    Changing comments and removing unnecessary newlines.

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -30,8 +30,9 @@ import Activities from "../pages/Activities/Activities";
 import ActivitiesDetail from "../pages/Activities/details/Detail";
 import LoginForm from "../pages/login/LoginForm";
 import { Register } from "../pages/register/Register";
-// we need to map the `scale` prop we define below
-// to the transform style property
+
+
+// We need to map the `scale` prop we define below to the transform style property:
 function mapStyles(styles) {
   return {
     opacity: styles.opacity,
@@ -39,7 +40,7 @@ function mapStyles(styles) {
   };
 }
 
-// wrap the `spring` helper to use a bouncy config
+// Wrap the `spring` helper to use a bouncy config:
 function bounce(val) {
   return spring(val, {
     stiffness: 330,
@@ -47,19 +48,19 @@ function bounce(val) {
   });
 }
 
-// child matches will...
+// Child matches will...
 const bounceTransition = {
-  // start in a transparent, upscaled state
+  // Start in a transparent, upscaled state:
   atEnter: {
     opacity: 0,
     scale: 1.2,
   },
-  // leave in a transparent, downscaled state
+  // Leave in a transparent, downscaled state:
   atLeave: {
     opacity: bounce(0),
     scale: bounce(0.8),
   },
-  // and rest at an opaque, normally-scaled state
+  // And rest at an opaque, normally-scaled state:
   atActive: {
     opacity: bounce(1),
     scale: bounce(1),
@@ -70,14 +71,13 @@ const AppRouter = () => {
   return (
     <Router>
       <div>
-        <AnimatedSwitch
-          atEnter={bounceTransition.atEnter}
-          atLeave={bounceTransition.atLeave}
-          atActive={bounceTransition.atActive}
-          mapStyles={mapStyles}
-          className="route-wrapper"
-        >
-          {/* public */}
+        <AnimatedSwitch atEnter={bounceTransition.atEnter}
+                        atLeave={bounceTransition.atLeave}
+                        atActive={bounceTransition.atActive}
+                        mapStyles={mapStyles}
+                        className="route-wrapper">
+
+          {/* PUBLIC */}
           <Route exact path="/" component={Home} />
           <Route exact path="/novedades" component={News} />
           <Route exact path="/novedades/:id" component={NewsDetail} />
@@ -88,90 +88,31 @@ const AppRouter = () => {
           <Route exact path="/actividades/:id" component={ActivitiesDetail} />
           <Route exact path="/login" component={LoginForm} />
           <Route exact path="/register" component={Register} />
-          {/* backoffice */}
+
+          {/* BACKOFFICE */}
           <Route exact path="/backoffice" component={Backoffice} />
           <Route exact path="/backoffice/home" component={HomeForm} />
-          <Route
-            exact
-            path="/backoffice/activities"
-            component={ActivitiesList}
-          />
-          <Route
-            exact
-            path="/backoffice/activities/create"
-            component={FormActivities}
-          />
-          <Route
-            exact
-            path="/backoffice/activities/edit"
-            component={FormActivities}
-          />
-          <Route
-            exact
-            path="/backoffice/categories"
-            component={CategoriesBackOffice}
-          />
-          <Route
-            exact
-            path="/backoffice/categories/create "
-            component={CategoriesForm}
-          />
-          <Route
-            exact
-            path="/backoffice/categories/edit "
-            component={CategoriesForm}
-          />
+          <Route exact path="/backoffice/activities" component={ActivitiesList} />
+          <Route exact path="/backoffice/activities/create" component={FormActivities} />
+          <Route exact path="/backoffice/activities/edit" component={FormActivities} />
+          <Route exact path="/backoffice/categories" component={CategoriesBackOffice} />
+          <Route exact path="/backoffice/categories/create " component={CategoriesForm} />
+          <Route exact path="/backoffice/categories/edit " component={CategoriesForm} />
           <Route exact path="/backoffice/members" component={MemberList} />
-          <Route
-            exact
-            path="/backoffice/members/create"
-            component={MemberForm}
-          />
+          <Route exact path="/backoffice/members/create" component={MemberForm} />
           <Route exact path="/backoffice/members/edit" component={MemberForm} />
           <Route exact path="/backoffice/news" component={NewsScreen} />
-          <Route
-            exact
-            path="/backoffice/news/create"
-            component={FormNovedades}
-          />
-          <Route
-            exact
-            path="/backoffice/news/edit/:id"
-            component={FormNovedades}
-          />
-          <Route
-            exact
-            path="/backoffice/organization"
-            component={Organization}
-          />
-          <Route
-            exact
-            path="/backoffice/organization/edit"
-            component={OrgForm}
-          />
-          <Route
-            exact
-            path="/backoffice/projects/create"
-            component={FormProjects}
-          />
-          <Route
-            exact
-            path="/backoffice/projects/edit"
-            component={FormProjects}
-          />
+          <Route exact path="/backoffice/news/create" component={FormNovedades} />
+          <Route exact path="/backoffice/news/edit/:id" component={FormNovedades} />
+          <Route exact path="/backoffice/organization" component={Organization} />
+          <Route exact path="/backoffice/organization/edit" component={OrgForm} />
+          <Route exact path="/backoffice/projects/create" component={FormProjects} />
+          <Route exact path="/backoffice/projects/edit" component={FormProjects} />
           <Route exact path="/backoffice/slides" component={SlidesScreen} />
           <Route exact path="/backoffice/slides/create" component={SlideForm} />
           <Route exact path="/backoffice/slides/edit" component={SlideForm} />
-          <Route
-            exact
-            path="/backoffice/testimonials/create"
-            component={TestimonialForm}
-          />
-          <Route
-            exact
-            path="/backoffice/testimonials/edit"
-            component={TestimonialForm}
-          />
+          <Route exact path="/backoffice/testimonials/create" component={TestimonialForm} />
+          <Route exact path="/backoffice/testimonials/edit" component={TestimonialForm} />
           <Route exact path="/backoffice/users" component={UsersList} />
           <Route exact path="/backoffice/users/create" component={UserForm} />
           <Route exact path="/backoffice/users/edit/:id" component={UserForm} />
