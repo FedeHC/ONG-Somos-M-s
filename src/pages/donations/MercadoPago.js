e0461ff1ec391c06a5725f63a1f6f5c687c5886d
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-20 22:18:25 -0300

    Added logic to inject information to the mercado pago  script

--- src/pages/donations/MercadoPago.js
+++ src/pages/donations/MercadoPago.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Mercadopago = () => {
+
+ const FORM_ID = "payment-button";
+
+ const preferenceId="1003539968-8ccba64d-116c-44b9-8aed-59aee3cd9f79";
+   useEffect(() => {
+    if (preferenceId) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src =
+      "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
+      script.setAttribute('data-preference-id', preferenceId);
+      const form = document.getElementById(FORM_ID);
+      form.appendChild(script);
+      
+    }
+  }, [preferenceId]);
+  
+
  return (
   <div>
    
