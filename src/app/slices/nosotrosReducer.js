909780df4edac805c700d2567d36fc5cc9278328
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-22 07:54:28 -0300

    getNosotros added to state nosotros

--- src/app/slices/nosotrosReducer.js
+++ src/app/slices/nosotrosReducer.js
@@ -1,4 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getHome } from '../../services/apiHome';
+
+export const getNosotros = createAsyncThunk(
+	"nosotros/getNosotros",
+	async () => {
+		return await getHome();
+	}
+);
+
 
 const nosotrosSlice = createSlice({
   name: 'nosotros',
@@ -7,7 +16,22 @@ const nosotrosSlice = createSlice({
     loading: null,
     error: null,
   },
-  reducers: {},
+  extraReducers:{
+   //get
+   [getNosotros.pending]:(state, action)=>{
+    state.loading = true;
+   },
+   [getNosotros.fulfilled]:(state, action)=>{
+    state.nosotros = action.payload.data.data;
+    state.loading = false;
+   },
+   [getNosotros.rejected]:(state, action)=>{
+    state.error = action.payload;
+    state.loading = false;
+   },
+
+  },
 });
 
 export default nosotrosSlice.reducer;
+
