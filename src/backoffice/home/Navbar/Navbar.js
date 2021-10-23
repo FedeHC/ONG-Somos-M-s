3d296a257ab2da2fa6ee0267a6628381a00c565a
Joel <teslavargas18@gmail.com>
2021-10-23 13:45:10 -0300

    navbar agregado

---
+++ src/backoffice/home/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'; 
+import { RiMenuUnfoldLine } from 'react-icons/ri'; 
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <RiMenuUnfoldLine className="icon" />
+        </div>
+    )
+}
+
+export default Navbar; 
