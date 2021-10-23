fe2d2a0f1a6aff5e84662b2464b727fcdaeade94
Juan <juanjoseborrelli92@outlook.com>
2021-10-23 16:33:34 -0300

    Creacion de accion asincronica para realizar el dispatch de la lista de slides

--- src/reducers/slidesReducer.js
+++ src/reducers/slidesReducer.js
@@ -1,3 +1,25 @@
-export const slidesReducer = () => {
-    
-}
+import { useListSlides } from '../services/slidesServices';
+import { types } from '../types/types';
+
+const initialState = null;
+
+export const slidesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SLIDES:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export const listSlides = () => {
+  return async dispatch => {
+    const { data } = await useListSlides(process.env.REACT_APP_ENDPOINT_SLIDES);
+
+    dispatch({
+      type: types.SLIDES,
+      payload: data,
+    });
+  };
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { slidesReducer } from "../reducers/slidesReducer";
+import { configureStore } from '@reduxjs/toolkit';
+import { slidesReducer } from '../reducers/slidesReducer';
 
 export default configureStore({
   reducer: {
-    slides: slidesReducer
+    slides: slidesReducer,
   },
 });
