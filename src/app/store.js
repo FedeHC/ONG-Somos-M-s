d3dc5fc2cf5b8ee56e3041c18f1d4d82ddaa8d7f
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-23 16:10:36 -0300

    actividadesReducer incorporated into the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import actividades from './actividades/actividadesReducer';
+
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    actividades
+  },
 });
