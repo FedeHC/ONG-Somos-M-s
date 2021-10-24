46e2442b3d252e8b9af2185e51cdefe82234ffdf
facc77 <ferrerfacundo96@gmail.com>
2021-10-24 15:32:50 -0300

    about page reestructured

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,32 +1,65 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Container, Flex, VStack, Heading, Text } from "@chakra-ui/react";
-import Title from "../../features/title/Title";
+import React from 'react';
+import './about.scss';
+import { Heading, Text } from '@chakra-ui/react';
+import Title from '../../features/title/Title';
+import LinkedinCard from './LinkedinCard';
+import { TwitterTweet } from 'react-social-plugins';
+import TweetEmbed from 'react-tweet-embed';
+import SocialFollow from './SocialFollow';
 
-
-const About = ({ mainTitle, sectionTitle, text }) => {
+const About = () => {
   return (
-    <ChakraProvider>
-      <Container>
-        <Flex>
-          <VStack width="full"
-                  spacing={10}
-                  padding={10}
-                  alignItems="center">
+    <>
+      <div className="aboutContainer">
+        <div className="linkedinCard">
+          <div className="card1">
+            <LinkedinCard />
+          </div>
+          <div className="cardText">
+            <Heading
+              lineHeight={1.1}
+              fontWeight={400}
+              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+            >
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: '30%',
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'linkedin.400',
+                  zIndex: -1,
+                }}
+              >
+                Nueva Cuenta!
+              </Text>
+            </Heading>
+            <Text fontSize="3xl" color={'gray.500'}>
+              Con el objetivo de llegar a más gente que quiera colaborar en este
+              hermoso proyecto, creamos una nueva cuenta en Linkedin, siguenos!
+            </Text>
+          </div>
+        </div>
+        <div className="info">
+          <SocialFollow />
+        </div>
 
-            {/* MAIN TITLE */}
-            <Title text={mainTitle} />
-            
-            {/* SECTION TITLE */}
-            <Heading size="xl">{sectionTitle}</Heading>
-
-            {/* SECTION PARAGRAPH */}
-            <Text paddingBottom="10%">{text}</Text>
-
-          </VStack>
-        </Flex>
-      </Container>
-    </ChakraProvider>
+        <div className="tweetsSection">
+          <Text fontSize="3xl" color={'blue.400'} as={'span'}>
+            Nuestros últimos tweets!!
+          </Text>{' '}
+          <div className="tweets">
+            <TweetEmbed className="tweet" id="1452017848822509573" />
+            <TweetEmbed className="tweet" id="1452066835927556099" />
+            <TweetEmbed className="tweet" id="1452000630755659777" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
