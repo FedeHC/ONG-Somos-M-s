c6de7fb9d0bf60a4e6bddd78f2364489487b75eb
facc77 <ferrerfacundo96@gmail.com>
2021-10-29 00:04:40 -0300

    chackra alert fixed

---
+++ src/features/errorAlert/errorAlert.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+} from '@chakra-ui/react';
+
+import { Box } from '@chakra-ui/react';
+
+const ErrorAlert = () => {
+  return (
+    <Box
+      height="50vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Alert w="70%" status="error">
+        <AlertIcon mr={4} />
+
+        <AlertTitle mr={2}>Oops! Ocurrió un error!</AlertTitle>
+
+        <AlertDescription>Recarga la página por favor!</AlertDescription>
+      </Alert>
+    </Box>
+  );
+};
+
+export default ErrorAlert;
