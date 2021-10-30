7688d0c82bddada235f0ef60f0b94f6a08dac2c0
facc77 <ferrerfacundo96@gmail.com>
2021-10-30 20:01:04 -0300

    styles added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,35 +1,62 @@
 import React from 'react';
+import './home.css';
+import { Box } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/button';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import CardNews from '../../features/cardNews';
 import Slider from './SliderHome.js';
 import { useSelector, useDispatch } from 'react-redux';
-import {setNovedadDetail} from "../../app/novedades/novedadesReducer"
+import { setNovedadDetail } from '../../app/novedades/novedadesReducer';
 import { Link } from 'react-router-dom';
-import './home.css';
+import { Spinner } from '../../features/spinner';
+import { Skeleton } from '@chakra-ui/react';
+import ErrorAlert from '../../features/errorAlert/errorAlert';
 
 const Home = ({ history }) => {
   const dispatch = useDispatch();
 
-  const { loading:loadingNosotros, error:errorNosotros, nosotros } = useSelector(state => state.nosotros);
+  const {
+    loading: loadingNosotros,
+    error: errorNosotros,
+    nosotros,
+  } = useSelector(state => state.nosotros);
   const { loading, error, novedadesList } = useSelector(
     state => state.novedades,
   );
-  const callDispatch = (news) =>{
-     dispatch(setNovedadDetail(news));
-  }
+  const callDispatch = news => {
+    dispatch(setNovedadDetail(news));
+  };
 
   return (
     <div>
-      {!loading && <h1 className="title__main">{nosotros.welcome_text}</h1>}
+      {loading ? (
+        <Box
+          height="5rem"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Skeleton width="80%" mt={'2rem'} padding="3rem" />
+        </Box>
+      ) : nosotros ? (
+        <h1 className="title__main">{nosotros.welcome_text}</h1>
+      ) : (
+        <ErrorAlert />
+      )}
       <Slider />
 
       <section className="news__container">
         <h3>Ultimas Novedades</h3>
         <div className="news__flex">
           {loading ? (
-            <div>cargando...</div>
-          ) : (
+            <>
+              <>
+                <Skeleton className="skeletonNews" />
+                <Skeleton className="skeletonNews" />
+                <Skeleton className="skeletonNews" />
+              </>
+            </>
+          ) : novedadesList ? (
             novedadesList.map(news => (
               <Link key={news.id} to={`novedades/${news.id}`}>
                 <div onClick={() => callDispatch(news)}>
@@ -37,6 +64,8 @@ const Home = ({ history }) => {
                 </div>
               </Link>
             ))
+          ) : (
+            <ErrorAlert />
           )}
         </div>
         <div className="btn__container">
