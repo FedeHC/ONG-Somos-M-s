5fbfd0474a95ae4ba62200a4682626741135ce1f
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-11-02 14:03:55 -0300

    added input search

--- src/pages/Activities/Activities.js
+++ src/pages/Activities/Activities.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Activities.css';
 import CardNews from '../../features/cardNews/';
-import Card from '../../features/card/Card';
-import { Heading, Text } from '@chakra-ui/react';
+import { FormControl,
+  Stack,
+  Input,
+  useColorModeValue, } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -27,8 +29,36 @@ const News = () => {
       <div style={{ width: '95vw' }}>
         <TitleScreen title={'Actividades'} />
       </div>
+      
 
       <div className="containerCard">
+      <Stack
+          direction={{ base: 'column', md: 'row' }}
+          as={'form'}
+          spacing={'12px'}
+          width={'100%'}
+          me={6}
+          ms={6}
+          mb={6}
+        >
+          <FormControl>
+            <Input
+              variant={'solid'}
+              width="100%"
+              borderWidth={1}
+              color={'gray.800'}
+              _placeholder={{
+                color: 'gray.400',
+              }}
+              borderColor={useColorModeValue('#00214D', 'gray.700')}
+              id={'email'}
+              type={'text'}
+              autoComplete="off"
+              placeholder={'Buscar...'}
+              aria-label={'Buscar...'}
+            />
+          </FormControl>
+        </Stack>
         {loading ? (
           <Spinner />
         ) : actividadesList ? (
