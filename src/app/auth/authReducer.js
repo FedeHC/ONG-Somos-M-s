b625ce805382a901b75238497150e0ed0557a9e6
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-11-05 00:36:14 -0300

    login fixed with api alkemy

--- src/app/auth/authReducer.js
+++ src/app/auth/authReducer.js
@@ -1,12 +1,21 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import { loginUser, revalidationUser } from '../../services/apiAuth';
+import { loginUser, revalidationUser, registerUSer } from '../../services/apiAuth';
 
 
 export const startLogin = createAsyncThunk(
 	"auth/startLogin",
 	async (body) => {
-		  const resp = await loginUser("auth/login", body);
-      localStorage.setItem("token", resp.data.data.token);
+		  const resp = await loginUser("login", body);
+      localStorage.setItem("token",resp.data.data.data.token);
+      return resp;
+    });
+
+    
+export const startRegister = createAsyncThunk(
+	"auth/startRegister",
+	async (body) => {
+		  const resp = await registerUSer("register", body);
+      localStorage.setItem("token",resp.data.data.data.token);
       return resp;
     });
     
@@ -15,8 +24,7 @@ export const startLogin = createAsyncThunk(
 export const startRenew = createAsyncThunk(
   "auth/startRenew",
   async ()=>{
-    const resp = await revalidationUser("auth/renew");
-    localStorage.setItem("token", resp.data.data.token);
+    const resp = await revalidationUser("auth/me");
     return resp;
   });
 
@@ -48,8 +56,8 @@ const authSlice = createSlice({
     [startLogin.fulfilled]:(state, action)=>{
        state.loading = false;
        if(!action.payload.error){
-         state.user = action.payload.data.data.usuario;
-         state.token = action.payload.data.data.token;
+         state.user = action.payload.data.data.data.user;
+         state.token = action.payload.data.data.data.token;
         }else{
           state.error = action.payload.error;
         }
@@ -68,17 +76,15 @@ const authSlice = createSlice({
     },
     [startRenew.fulfilled]:(state, action)=>{
        state.loading = false;
-       if(!action.payload.error){
-         state.user = {
-            name: action.payload.data.data.name,
-            uid:action.payload.data.data.uid
-         }
-         state.token = action.payload.data.data.token;
+       if(action.payload.data.data.success){
+         state.user = action.payload.data.data.data.user;
+         state.token = localStorage.getItem("token");
+         state.logged = true;
         }else{
           state.error = action.payload.error;
+          state.logged = false;
         }
         state.checking = false;
-        state.logged = true;
     },
     [startRenew.rejected]:(state, action)=>{
        state.loading = false;
